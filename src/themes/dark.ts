e953514a4a56ae2cc0ff1b684baa8037ed3687e7
@erick <erick.hx22@gmail.com>
2023-02-21 16:21:03 -0500

    chore: add global/dark/light theme

---
+++ src/themes/dark.ts
@@ -0,0 +1,22 @@
+const darkTheme = {
+  colors: {
+    // background: "#282a36",
+    background: "#121212",
+    currentLine: "#44475a",
+    foreground: "#f8f8f2",
+    commnet: "#6272a4",
+    cyan: "#8be9fd",
+    green: "#50fa7b",
+    orange: "#ffb86c",
+    pink: "#ff79c6",
+    purple: "#bd93f9",
+    red: "#ff5555",
+    yellow: "#f1fa8c",
+    white: "#fff",
+    textColor: "#fff",
+    light: "#fff",
+  },
+};
+
+export type DarkThemeType = typeof darkTheme;
+export default darkTheme;
