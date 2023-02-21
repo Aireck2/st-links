bd57de3f67b2d09af3ae5b2d12dc411ef3e7cfbe
@erick <erick.hx22@gmail.com>
2023-02-21 16:24:35 -0500

    chore: add social constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,19 @@
+export const constants = {
+  social: [
+    {
+      name: "github",
+      src: "/icons/github.svg",
+      url: "https://github.com/aireck2",
+    },
+    {
+      name: "linkedin",
+      src: "/icons/linkedin.svg",
+      url: "https://www.linkedin.com/in/erickescriba/",
+    },
+    {
+      name: "gmail",
+      src: "/icons/mail.svg",
+      url: "mailto:erickescribaa@gmail.com",
+    },
+  ],
+};
