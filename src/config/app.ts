13d2a2eb168c6367e40902be8fec38157b5771e3
@erick <erick.hx22@gmail.com>
2023-02-22 12:29:59 -0500

    feat: add api spotify/github env vars

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,5 +1,15 @@
-export const {
+const {
   SPOTIFY_CLIENT_ID: clientId,
   SPOTIFY_CLIENT_SECRET: clientSecret,
   SPOTIFY_REFRESH_TOKEN: refreshToken,
+  API_SPOTIFY: apiSpotify,
+  API_GITHUB: apiGithub,
+  USER_GITHUB: userGithub,
 } = process.env;
+
+const config = {
+  spotify: { clientId, clientSecret, refreshToken, api: apiSpotify },
+  github: { api: apiGithub, user: userGithub },
+};
+
+export default config;
