fe4ced402eb5586604fc9ae3ac4fab6ef411f0c0
@erick <erick.hx22@gmail.com>
2023-03-03 12:48:55 -0500

    chore: add github/linkedin/mail icon components (#5)

---
+++ src/components/basics/Linkedin.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { FC } from "react";
+import { IconProps } from "@/types/components";
+
+const Linkedin: FC<IconProps> = ({ color = "#ffffff", width = 20 }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      width={width}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-linkedin"
+    >
+      <title>LinkedIn</title>
+      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+      <rect x="2" y="9" width="4" height="12"></rect>
+      <circle cx="4" cy="4" r="2"></circle>
+    </svg>
+  );
+};
+
+export default Linkedin;
