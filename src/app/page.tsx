44bcc3ec7c43bfeff379e8c748075f13a8bfcb1d
@erick <erick.hx22@gmail.com>
2023-03-03 13:00:10 -0500

    refactor: add spotify player (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
 "use client";
-import useSWR from "swr";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 import { App } from "@/components/layouts/App";
-import { ThemeProvider } from "@/context/ThemeProvider";
+
 import { Switch } from "@/components/basics";
-import { SpotifyStatus, Heading } from "@/components/screens";
+import { SpotifyPlayer, Heading } from "@/components/screens";
 
 export default function Home() {
-  const fetcher = (url: string) => fetch(url).then((r) => r.json());
-  const { data } = useSWR("/api/spotify", fetcher);
-
   return (
     <ThemeProvider>
       <App>
-        <Heading />
         <Switch />
-        {!data ? <></> : <SpotifyStatus data={data} />}
+        <Heading />
+        <SpotifyPlayer />
       </App>
     </ThemeProvider>
   );
