ed6103e5e1c38f93a07f94c2a4cfee6d5ad3b6f7
@erick <erick.hx22@gmail.com>
2023-03-03 12:54:28 -0500

    chore: add social links component (#5)

---
+++ src/components/screens/SocialLinks/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+export const SocialStyled = styled.div`
+  margin-top: 20px;
+  display: flex;
+  grid-gap: 20px;
+  min-width: 200px;
+  justify-content: center;
+  & a:hover {
+    transform: scale(1.075);
+  }
+`;
