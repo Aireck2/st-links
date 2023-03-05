309f8e2854cc1a9d9d541e701c7c977b3d6c7a8a
@erick <erick.hx22@gmail.com>
2023-03-05 11:46:35 -0500

    refactor: add metadata & add page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,35 @@
-"use client";
-import { ThemeProvider } from "@/context/ThemeProvider";
-
-import { App } from "@/components/layouts/App";
-
 import { Switch } from "@/components/basics";
 import { SpotifyPlayer, Heading } from "@/components/screens";
 
+export const metadata = {
+  title: "Erick Escriba | Links",
+  description: "Erick Escriba's social media & spotify player status",
+  viewport: "width=device-width, initial-scale=1",
+  keywords: ["Erick", "Social media", "Links", "linkinbio"],
+  authors: [{ name: "Erick Escriba", url: "https://erickescribaa.vercel.app" }],
+  openGraph: {
+    title: "Erick Escriba",
+    description: "Erick Escriba's social media & spotify player status",
+    url: "https://aireck-linkinbio.vercel.app",
+    siteName: "Erick Escriba | Links",
+    images: [
+      {
+        url: "https://aireck-linkinbio.vercel.app/favicon.ico",
+        width: 256,
+        height: 256,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+};
+
 export default function Home() {
   return (
-    <ThemeProvider>
-      <App>
-        <Switch />
-        <Heading />
-        <SpotifyPlayer />
-      </App>
-    </ThemeProvider>
+    <>
+      <Switch />
+      <Heading />
+      <SpotifyPlayer />
+    </>
   );
 }
