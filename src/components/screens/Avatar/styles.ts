4a393de8848d46ed2b37f19eebabf95a99dccd41
@erick <erick.hx22@gmail.com>
2023-04-13 17:16:15 -0500

    feat: add not avatar styles (#9)

--- src/components/screens/Avatar/styles.ts
+++ src/components/screens/Avatar/styles.ts
@@ -5,7 +5,15 @@ import styled from "styled-components";
 export const AvatarStyled = styled(Image)`
   width: 120px;
   height: 120px;
+  background: #000;
   border-radius: 50%;
   object-fit: fill;
   object-position: center;
+`;
+
+export const NotImage = styled.div`
+  width: 120px;
+  height: 120px;
+  background: #000;
+  border-radius: 50%;
 `;
