473c0c6c4c9eeb7c0924cba73d2d39fe6963a5a5
@erick <erick.hx22@gmail.com>
2023-04-13 16:50:09 -0500

    chore: update not playing styles

--- src/components/screens/SpotifyPlayer/styled/NotPlaying.ts
+++ src/components/screens/SpotifyPlayer/styled/NotPlaying.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+const borderRadius = "1rem";
+
 export const NotPlayingStyled = styled.section`
   display: flex;
   justify-content: center;
@@ -8,23 +10,27 @@ export const NotPlayingStyled = styled.section`
     place-items: center;
     padding: 1rem;
     background-color: #212121;
-    border-top-left-radius: 2rem;
-    border-bottom-left-radius: 2rem;
-    @media screen and (min-width: 480px) {
+    border-top-left-radius: ${borderRadius};
+    border-bottom-left-radius: ${borderRadius};
+    /* @media screen and (min-width: 480px) {
       padding: 2rem;
-    }
+    } */
   }
   .content {
     padding: 1rem;
     background-color: #121212;
-    border-top-right-radius: 2rem;
-    border-bottom-right-radius: 2rem;
-    @media screen and (min-width: 480px) {
+    border-top-right-radius: ${borderRadius};
+    border-bottom-right-radius: ${borderRadius};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    /* @media screen and (min-width: 480px) {
       padding: 2rem;
-    }
+    } */
 
     p {
       color: #b3b3b3;
+      font-size: clamp(0.8rem, 3vw, 1rem);
     }
   }
 `;
