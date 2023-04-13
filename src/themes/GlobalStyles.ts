5cfddd0e3a79398086defe9e6acae842aa9bc0c0
@erick <erick.hx22@gmail.com>
2023-04-13 16:51:50 -0500

    feat: add sora next/font in global styles

--- src/themes/GlobalStyles.ts
+++ src/themes/GlobalStyles.ts
@@ -1,13 +1,16 @@
+import { Sora } from "next/font/google";
 import { createGlobalStyle } from "styled-components";
 
 import { DarkThemeType } from "./dark";
 import { LightThemeType } from "./light";
 
+const sora = Sora({ subsets: ["latin"] });
+
 export const GlobalStyles = createGlobalStyle<{
   theme: DarkThemeType & LightThemeType;
 }>`
    body {
-     font-family: sans-serif;
+     font-family: ${sora.style.fontFamily};
      background: ${({ theme }) => theme.colors.background};
      color: ${({ theme }) => theme.colors.white};
      transition: all 0.50s linear; 
