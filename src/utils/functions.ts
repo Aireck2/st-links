411c68d3db9ce117c843656482526c020cd690ca
@erick <erick.hx22@gmail.com>
2023-04-14 12:30:38 -0500

    fix: repair sec format (#12)

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,5 +2,5 @@ export const toMinSec = (ms: number) => {
   const min = Math.floor((ms / 1000 / 60) << 0);
   const sec = Math.floor((ms / 1000) % 60);
 
-  return `${min}:${sec}`;
+  return `${min}:${sec.toString().padStart(2, "0")}`;
 };
