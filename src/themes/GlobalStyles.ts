77752fddde1738076b6b3db31e5bdb78ca2338a9
@erick <erick.hx22@gmail.com>
2023-04-20 16:10:59 -0500

    chore: add scrollbar styles (#18)

--- src/themes/GlobalStyles.ts
+++ src/themes/GlobalStyles.ts
@@ -13,6 +13,23 @@ export const GlobalStyles = createGlobalStyle<{
      font-family: ${sora.style.fontFamily};
      background: ${({ theme }) => theme.colors.background};
      color: ${({ theme }) => theme.colors.white};
-     transition: all0.22s cubic-bezier(0.55, 0.055, 0.675, 0.19),
+     transition: all 0.22s cubic-bezier(0.55, 0.055, 0.675, 0.19)
+  }
+
+  @media (orientation: landscape){
+   ::-webkit-scrollbar {
+      width: 12px;
+      height: 12px;
+      background-color: hsl(210deg, 14%, 66%);
+   }
+   ::-webkit-scrollbar-thumb {
+      border-radius: 5px;
+      background-color: hsl(210deg, 14%, 66%);
+      border: 2px solid hsl(210deg, 15%, 20%);
+   }
+
+   ::-webkit-scrollbar-track {
+      background-color: hsl(210deg, 15%, 20%);
+    }
   }
 `;
